3b8592ce4a9584489d1a47e74aa7c157e3e760f6
langzilangzi <1874815111@qq.com>
2022-07-28 17:01:44 +0800

    :white_check_mark: add files about switch button

--- src/components/LikeButtion.tsx
+++ src/components/LikeButtion.tsx
@@ -1,10 +1,16 @@
 import React, { useState } from "react";
 const LikeButtion: React.FC = () => {
     const [like, setLike] = useState(0)
+    const [on, setOn] = useState(true)
     return (
+        <>
         <button onClick={() => {setLike(like + 1)}}>
             {like} 👍
         </button>
+        <button onClick={() => {setOn(!on)}}>
+            { on ? 'ON' : 'OFF' }
+        </button>
+        </>
     )
 }
 
